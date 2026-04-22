4a92fb6c00ed58104eea3a33bc950e896b4fb08a
Raymond Munguti <raymondmunguti163@gmail.com>
2026-04-22 09:19:09 +0300

    fix: replace SALT_ROUNDS constant with direct value in hashPassword function

--- server/src/utils/password.js
+++ server/src/utils/password.js
@@ -1,9 +1,7 @@
 const bcrypt = require('bcryptjs');
 
-const SALT_ROUNDS = 10;
-
 async function hashPassword(plain) {
-  return bcrypt.hash(plain, SALT_ROUNDS);
+  return bcrypt.hash(plain, 10);
 }
 
 async function comparePassword(plain, hash) {
